d99293dcfd17e5277bc89bf97d68518f133f06aa
danifaus89 <50027294+danifaus89@users.noreply.github.com>
2023-03-10 11:36:06 +0100

    Delete Back directory

--- Back/routes/movements.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { deleteMovements } = require("../controllers/movements.controller");
-const movementsRouter = Router();
-
-//MOVEMENTS
-movementsRouter.post("/", deleteMovements);
-
-module.exports = movementsRouter;
